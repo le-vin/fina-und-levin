ac85b272b22494fc471930fa17a41278362f5b2b
Levin Bauschke <b.bauschke@zeppelin-university.net>
2022-06-27 12:24:19 +0200

    DOne

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'home', to: 'pages#home'
-  root 'pages#home'
 end
